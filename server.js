ee6dc879f927d8e5dc82656d7bb65ec96beb5853
Jeffery Davis <jef.davis@live.com>
2021-02-22 16:59:35 -0600

    fixed missing parts

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var PORT = process.eventNames.PORT || 8080;
+var PORT = process.eventNames.PORT || 5000;
 var app = express();
 
 app.use(express.static("public"));
